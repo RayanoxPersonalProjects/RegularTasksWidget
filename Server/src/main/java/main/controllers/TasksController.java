b98298b76a6534015bce1e0ec3bdaebe4e52e3a9
Rayane BEN-HMIDANE <rayane.benhmidane32@hotmail.fr>
2019-05-27 16:03:35 +0200

    Add custom authentication (by token)

--- Server/src/main/java/main/controllers/TasksController.java
+++ Server/src/main/java/main/controllers/TasksController.java
@@ -1,19 +1,27 @@
 package main.controllers;
 
+import org.apache.tomcat.websocket.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import main.Authentication.ServerTokenProvider;
 import model.AllTasksResults;
 import model.OperationResult;
 
 @RestController
 public class TasksController {
 
+	@Autowired
+	ServerTokenProvider serverTokenProvider;
 
     @RequestMapping(value = "/retrieveAllTasks", method = RequestMethod.GET)
-    public AllTasksResults RetrieveAllTasks() {
+    public AllTasksResults RetrieveAllTasks(@RequestParam(value = "token") String token) throws AuthenticationException {
+    	OperationResult failedAuth = processAuthorization(token);
+    	if(failedAuth != null)
+    		return new AllTasksResults(failedAuth.getErrorMessage());
     	
     	String [] allTasksOfCurrentDay;
     	
@@ -27,7 +35,12 @@ public class TasksController {
         return new AllTasksResults(allTasksOfCurrentDay);
     }
     
-    public OperationResult UpdateTasks(@RequestParam(value = "taskTitle") String taskTitle, @RequestParam(value = "isCompleted") Boolean isCompleted) {
+    public OperationResult UpdateTasks(@RequestParam(value = "taskTitle") String taskTitle, @RequestParam(value = "isCompleted") Boolean isCompleted, @RequestParam(value = "token") String token) throws AuthenticationException {
+    	OperationResult failedAuth = processAuthorization(token);
+    	if(failedAuth != null)
+    		return failedAuth;
+    	
+    	
     	if(taskTitle == null || isCompleted == null) {
     		return getFailedOperationResult("At least one of the input parameters is null"); 
     	}
@@ -51,6 +64,16 @@ public class TasksController {
     /*
      *  Private tools
      */
+    
+
+    
+    private OperationResult processAuthorization(String token) throws AuthenticationException {
+    	if(!serverTokenProvider.AuthorizeRequestToken(token)) {
+    		return new OperationResult(false, "The token does not equal the server-side token.");
+    	}else {
+    		return null;
+    	}
+    }
     
     
     private OperationResult getFailedOperationResult(String errorMessage) {
